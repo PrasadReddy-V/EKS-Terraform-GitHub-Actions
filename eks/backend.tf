3fcac14b2f700dc17eddca65764efea8ccb7fe89
PrasadReddy-V <86288145+PrasadReddy-V@users.noreply.github.com>
2025-04-18 10:19:08 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-aman-tf-bucket3"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     use_lockfile = true
